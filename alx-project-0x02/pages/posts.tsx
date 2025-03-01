502751c12dfadd66a578d256f80f2b1c2aca1181
Bismark Adzai <116949093+Bismark004@users.noreply.github.com>
2025-03-01 10:58:18 +0000

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PostCard from "@/components/common/PostCard";
 import {PostProps} from "@/interfaces/index";
+import Header from", "@/components/layout/Header";
 
 
 const Posts: React.FC = () => {
@@ -22,9 +23,8 @@ const Posts: React.FC = () => {
 
   return (
     <div>
-      <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-3xl font-bold text-center">Posts</h1>
-      </header>
+      <Header/>
+      
       <div className="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((post) => (
           <PostCard
